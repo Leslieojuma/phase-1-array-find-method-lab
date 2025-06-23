2459e21e4cf760bf02739a1cfad707211bc1ec67
leslie <leslie.ojuma@student.moringaschool.com>
2025-06-23 14:45:49 +0300

    2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
+       const record = [
+            { year: "2015", result: "W" },
+            { year: "2014", result: "N/A" },
+            { year: "2013", result: "L" },
+        ]
+    
+    
     function superbowlWin(record) {
-        const win = record.find(game => game.result === "W");
-        return win ? win.year : undefined;
+     const winRecord = record.find(game => game.result === "W");
+     return winRecord ? winRecord.year : undefined;
     }
+
 
